34d593bfccbe7cc43cf96f342aa1dc3494fb017d
barakus <diegocallediraldo@gmail.com>
2023-08-25 11:58:04 +0200

    secondPage und js file

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-# my js
+let navPanel = document.getElementById("navPanel");
+let pageTwo = document.getElementById("pageAboutme")
+let pageOne = document.getElementById("pageOne")
+
+
+pageTwo.addEventListener("mouseover", function(){
+    navPanel.style.transition = "1s"
+    navPanel.style.transform = "translate(0%)"
+})
+
+pageOne.addEventListener("mouseover", function(){
+    navPanel.style.transition = "1s"
+    navPanel.style.transform = "translate(-100%)"
+})
